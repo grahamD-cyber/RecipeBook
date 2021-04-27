dbf70f0c41bb1b2805d470c4731a6a4627e0e9d6
Graham Desmond <gdesmond@pdx.edu>
2021-04-27 16:38:48 -0700

    Fixing heroku issues

---
+++ index.js
@@ -0,0 +1,14 @@
+const PORT = process.env.PORT || 5000;
+const express = require("express");
+const INDEX = "/index.html";
+
+var app = express();
+var server = app.listen(PORT, () => {
+    console.log(`Book is running on port ${PORT}`);
+  
+  });
+
+app.get("/", (req, res) => {
+    res.sendFile(INDEX, { root: __dirname });
+  });
+
