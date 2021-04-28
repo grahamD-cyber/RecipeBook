32f74b28541fe6fac40134f90cffb2f1c347ca57
Graham Desmond <gdesmond@pdx.edu>
2021-04-28 10:59:54 -0700

    Got the starter website working

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const INDEX = "public/index.html";
 
 var app = express();
 var server = app.listen(PORT, () => {
-    console.log(`Book is running on port ${PORT}`);
+    console.log(`Recipe Book is running on port ${PORT}`);
   
   });
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(INDEX, { root: __dirname });
