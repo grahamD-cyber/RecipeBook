21a16fe381a402d01968bd12fb838a711c5ce8e7
Graham Desmond <gdesmond@pdx.edu>
2021-04-28 11:18:26 -0700

    Added all necessary pages and rudimentary routing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const PORT = process.env.PORT || 5000;
 const express = require("express");
 const INDEX = "public/index.html";
+const BROWSE = "public/browse.html";
+const RECIPEPAGE = "public/recipePage.html";
+const ADDRECIPE = "public/addRecipe.html";
+const SEARCHRESULTS = "public/searchResults.html";
 
 var app = express();
 var server = app.listen(PORT, () => {
@@ -12,5 +16,16 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(INDEX, { root: __dirname });
-  });
-
+});
+app.get("/search", (req, res) => {
+    res.sendFile(SEARCHRESULTS, { root: __dirname });
+});
+app.get("/browse", (req, res) => {
+    res.sendFile(BROWSE, { root: __dirname });
+});
+app.get("/recipe", (req, res) => {
+    res.sendFile(RECIPEPAGE, { root: __dirname });
+});
+app.get("/add", (req, res) => {
+    res.sendFile(ADDRECIPE, { root: __dirname });
+});
