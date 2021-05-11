98359ae2704fd0b6a09349e9118976053ca91195
Chinmay Tawde <chinmaytawde15@gmail.com>
2021-05-11 11:21:28 -0700

    Added Postgres dependency and its route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { Client } = require('pg');
 
 var app = express();
 
@@ -7,9 +8,26 @@ app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
 
 app.use(express.static("public"));
 
-app.get("/*", (req, res) => {
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+
+app.get("/", (req, res) => {
     res.sendFile(path.resolve("public", "index.html"));
 })
 
+app.get("/db", (req, res) => {
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
+})
 
  
