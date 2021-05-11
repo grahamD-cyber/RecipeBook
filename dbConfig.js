fc730fb1bdca0291028f5cce8b5dc67c85854899
Chinmay Tawde <chinmaytawde15@gmail.com>
2021-05-11 12:31:36 -0700

    Added global dbConfig module, which will be used to fetch data from Postgres Database

---
+++ dbConfig.js
@@ -0,0 +1,14 @@
+const {Pool} = require('pg');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+});
+
+module.exports.getPool = pool;
--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const express = require("express");
 const path = require("path");
-const { Client } = require('pg');
+const dbConfig = require("./dbConfig")
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 var app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
+app.listen(PORT, () => console.log(`Server running at port : ${PORT})`))
 
 app.use(express.static("public"));
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-  
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("public", "index.html"));
 })
 
-app.get("/db", (req, res) => {
-    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-          console.log(JSON.stringify(row));
+app.get("/db", async (req, res) => {
+    const POOL = dbConfig.getPool;
+    POOL.connect((err, client, release) => {
+        if (err) {
+            return console.error('Error acquiring client', err.stack)
         }
-        client.end();
-    });
+        client.query('SELECT * FROM public."Recipe" ;', (err, res) => {
+            release();
+            if (err) throw err;
+            for (let row of res.rows) {
+              console.log(JSON.stringify(row));
+            }
+        });
+    })
+    res.end();
 })
 
  
