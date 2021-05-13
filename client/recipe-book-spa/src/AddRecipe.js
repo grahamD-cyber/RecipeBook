2e2e9e4e26c02f8355ab1228b3d87a1a0011c339
Graham Desmond <gdesmond@pdx.edu>
2021-05-13 12:39:32 -0700

    Finished  add recipe page

--- client/recipe-book-spa/src/AddRecipe.js
+++ client/recipe-book-spa/src/AddRecipe.js
@@ -5,11 +5,68 @@ import DropdownButton from "./DropdownButton.js";
 class AddRecipe extends Component {
   constructor(props){
     super(props);
+    this.getData = this.getData.bind(this);
+    this.changeCategory = this.changeCategory.bind(this);
+    this.changeRegion = this.changeRegion.bind(this);
+    this.changeType = this.changeType.bind(this);
     this.state = {
+      mainCategory: "Choose a Category",
+      mainRegion: "Choose a Region",
+      mainType: "Choose a Type"
+    };
+  }
+  changeCategory(newValue) {
+      this.setState({mainCategory : newValue});
+  }
+
+  changeType(newValue) {
+    this.setState({mainType : newValue});
+  
+  }
+
+  changeRegion(newValue) {
+    this.setState({mainRegion : newValue});
+  }
+
+  getData(){
+    console.log("mealName: " + document.getElementById("mealName").value)
+    for (var i = 1; i <= 20; i++)
+    {
+      const id = "ingredient" + i
+      const name = "ingredient " + i + ": "
+      if (document.getElementById(id) != null)
+      {
+        console.log(name + document.getElementById(id).value)
+      }
+      else 
+      {
+        console.log(name + "null")
+      }
     }
-}
+    console.log("Youtube: " + document.getElementById("Youtube").value)
+    console.log("Tags: " + document.getElementById("Tags").value)
+    console.log("Instructions: " + document.getElementById("Instructions").value)
+    console.log("Region: " + this.state.mainRegion)
+    console.log("Category: " + this.state.mainCategory)
+    console.log("Type: " + this.state.mainType)
+    for (var j = 1; j <= 20; j++)
+    {
+      const id = "measure" + j
+      const name = "measure" + j + ": "
+      if (document.getElementById(id) != null)
+      {
+        console.log(name + document.getElementById(id).value)
+      }
+      else 
+      {
+        console.log(name + "null")
+      }
+    }
+  }
+
+
   render() {
-    var numIngredients = 4
+    var numIngredients = 4   
 
     function newIngredientClicked() {
       if (numIngredients <= 20)
@@ -18,7 +75,7 @@ class AddRecipe extends Component {
         div.classList.add('ingredientContainer');
         var num = String(numIngredients)
         div.innerHTML = `<input id = "ingredient${num}" class = "text-center ingredientInput" placeholder = "Ingredient ${num}"></input>
-        <span><input id = "measure${num} class = "text-center measureInput" placeholder = "Measure/Units"></input></span>`;
+        <span><input id = "measure${num}" class = "text-center measureInput" placeholder = "Measure/Units"></input></span>`;
         document.querySelector('.moreIngredients').appendChild(div);
         numIngredients = numIngredients + 1;
       }
@@ -58,9 +115,9 @@ class AddRecipe extends Component {
             <input id = "mealThumbnail"class = "text-center mainInput urlInput" placeholder = "Enter URL for Recipe Image"></input>
             <img class = "uploadImage" alt = "upload" src = "../images/photoIcon.png"/>
         </div>
-        <DropdownButton id = "Category" class = {"category"} dropdown = {"categoryDropdown"} dropdownContainer = {"categoryDropdownContainer"} mainButton={"categoryMain"} categoryText = {"Select a Category"} categories = {["Appetizers", "Beverages", "Soups","Salads", "Vegetables","Main Dishes","Breads", "Rolls","Desserts", "Sides", "Miscellaneous"]} imageId = "categoryImage" image = {"../images/categoryIcon.png"}/>
-        <DropdownButton id = "Region" class = {"region"} dropdown = {"regionDropdown"} dropdownContainer = {"regionDropdownContainer"} mainButton={"regionMain"} categoryText = {"Select a Region"} categories = {["Turkish","Italian","Chinese","Jamaican","Dutch","American","Tunisian","Spanish","Japanese","Canadian","Indian","Vietnamese","Portuguese","Moroccan","Unknown","Irish","French","Mexican","Thai","Malaysian","Kenyan","British","Egyptian","Greek","Polish","Russian"]} imageId = "regionImage" image = {"../images/regionIcon.png"}/>
-        <DropdownButton id = "Type" class = {"type"} dropdown = {"typeDropdown"} dropdownContainer = {"typeDropdownContainer"} mainButton={"typeMain"} categoryText = {"Select a Type"} categories = {["Vegetarian", "Non-Vegetarian"]} imageId = "typeImage" image = {"../images/typeIcon.png"}/>
+        <DropdownButton mainText = {this.state.mainCategory} changeData={this.changeCategory.bind(this)} id = "Category" class = {"category"} dropdown = {"categoryDropdown"} dropdownContainer = {"categoryDropdownContainer"} mainButton={"categoryMain"} categories = {["Appetizers", "Beverages", "Soups","Salads", "Vegetables","Main Dishes","Breads", "Rolls","Desserts", "Sides", "Miscellaneous"]} imageId = "categoryImage" image = {"../images/categoryIcon.png"}/>
+        <DropdownButton mainText = {this.state.mainRegion} changeData={this.changeRegion.bind(this)} id = "Region" class = {"region"} dropdown = {"regionDropdown"} dropdownContainer = {"regionDropdownContainer"} mainButton={"regionMain"} categories = {["Turkish","Italian","Chinese","Jamaican","Dutch","American","Tunisian","Spanish","Japanese","Canadian","Indian","Vietnamese","Portuguese","Moroccan","Unknown","Irish","French","Mexican","Thai","Malaysian","Kenyan","British","Egyptian","Greek","Polish","Russian"]} imageId = "regionImage" image = {"../images/regionIcon.png"}/>
+        <DropdownButton mainText = {this.state.mainType} changeData={this.changeType.bind(this)} id = "Type" class = {"type"} dropdown = {"typeDropdown"} dropdownContainer = {"typeDropdownContainer"} mainButton={"typeMain"} categories = {["Vegetarian", "Non-Vegetarian"]} imageId = "typeImage" image = {"../images/typeIcon.png"}/>
         <input id = "Tags" class = "mainInput tagsInput" placeholder = "Tags e.g.(chicken, pie, carrots, etc.)"></input>
         <div class = "mainInput">
             <input id = "Youtube" type = "url" class = "text-center mainInput youtubeInput" placeholder = "YouTube URL"></input>
@@ -68,7 +125,7 @@ class AddRecipe extends Component {
         </div>
         {/* <Tag/> */}
        
-        <button id = "submitButton" class = "text-center submitButton">Submit Recipe</button>
+        <button id = "submitButton" class = "text-center submitButton" onClick = {this.getData}>Submit Recipe</button>
       </div>
     );
   }
