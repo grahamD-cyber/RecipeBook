f79d111ba500ab69b44460f84b77efe74c4228a0
Chinmay Tawde <chinmaytawde15@gmail.com>
2021-05-25 12:07:52 -0700

    Removed MDB card dependencies

--- client/recipe-book-spa/src/Home.js
+++ client/recipe-book-spa/src/Home.js
@@ -73,7 +73,7 @@ class Home extends Component {
                             <div className = "searchContainer">
                                 <input onKeyDown={this.onKeyDown} className = "searchBox shadow" type="text" onChange={this.setTerm} placeholder= {this.state.placeholder}/>
                                 <button  className = "searchButton" onClick = {this.onClick}>
-                                    <NavLink id = "submit" class = {this.state.navClass} to={`/search/${this.state.searchTerm}`}><img className = "searchIcon" alt="SearchIcon" src = "/images/searchIcon.png"/></NavLink> 
+                                    <NavLink id = "submit" className = {this.state.navClass} to={`/search/${this.state.searchTerm}`}><img className = "searchIcon" alt="SearchIcon" src = "/images/searchIcon.png"/></NavLink> 
                                 </button>                
                             </div>
                         </div>
