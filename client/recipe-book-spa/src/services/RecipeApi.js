528fe3321be138e4d0d324fc69ea618629f18610
Graham Desmond <gdesmond@pdx.edu>
2021-05-25 14:45:27 -0700

    fixed opacity issues

--- client/recipe-book-spa/src/services/RecipeApi.js
+++ client/recipe-book-spa/src/services/RecipeApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const url = 'http://localhost:5000';
-const url = 'http://localhost:5000';
+const url = window.location.origin;
 
 export default axios.create({
     baseURL: `${url}`
